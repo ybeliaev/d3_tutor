eeda526196e47890f98ede4988354fb54cde377a
Jorgen <ybeliaev@gmail.com>
2021-03-25 12:06:06 +0200

    transferring code circle to react

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
 
 import './App.css';
 
+const width=960;
+const height=500;
+const centerX = width / 2
+const centerY = height / 2
+const strokeWidth = 10 
+const r = height / 2 - strokeWidth / 2
+
+const eyeOffsetX = 90
+const eyeOffsetY = 100
+const eyeRadius = 40
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <svg width="500" height="200">
+       <svg width={width} height={height}>
          <circle 
-          cx="250" 
-          cy="100"
-          r="100"
+          cx={centerX} 
+          cy={centerY}
+          r={r}
           fill="yellow"
-          ></circle>
+          stroke="black"
+          stroke-width={strokeWidth}
+          />
+          <circle
+          cx={centerX - eyeOffsetX} 
+          cy={centerY - eyeOffsetY}
+          r={eyeRadius}
+          fill="black"
+          />
+          <circle
+          cx={centerX + eyeOffsetX} 
+          cy={centerY - eyeOffsetY}
+          r={eyeRadius}
+          fill="black"
+          />
        </svg>
       </header>
     </div>
