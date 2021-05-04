6a048ff1375498ffe9aa061d9d9085488b1f8587
Jorgen <ybeliaev@gmail.com>
2021-05-04 13:13:09 +0300

    add data about array table of colors

--- src/components/csv_data/FetchCSV.js
+++ src/components/csv_data/FetchCSV.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as d3 from "d3"
 
 export default function FetchCSV() {
     // delete some numbers after raw:
@@ -13,12 +14,19 @@ export default function FetchCSV() {
         return await response.text()
         
     }
-    fetchText(csvURL).then(data => console.log(data))
+    fetchText(csvURL).then(text => {
+        const data = d3.csvParse(text)
+
+        console.log(Math.round(text.length/1024) + ' kB')
+        console.log(data.length + ' rows')
+        console.log(data.columns.length + ' columns')
+    })
   
     return (
         <div>
             <h2>Loading and parsing CSV data.</h2>
-            
+            <h3>Adding d3-dsv</h3>
+            <a href="https://github.com/d3/d3-dsv">Link d3-dsv</a>
         </div>
     )
 }
