c9e834dd458e97777735ac498c1f41d5a81e8619
Jorgen <ybeliaev@gmail.com>
2021-05-06 13:53:00 +0300

    add d3-dsv methods . output data into pre tag

--- src/components/csv_data/FetchCSV.js
+++ src/components/csv_data/FetchCSV.js
@@ -1,32 +1,47 @@
 import React from 'react'
 import * as d3 from "d3"
+import { doc } from 'prettier'
 
 export default function FetchCSV() {
     // delete some numbers after raw:
-    const csvURL = 'https://gist.githubusercontent.com/ybeliaev/e3e65a5135010936c4f359b9caf19260/raw/css_named_colors.csv'
+    const CSV_URL = 'https://gist.githubusercontent.com/ybeliaev/e3e65a5135010936c4f359b9caf19260/raw/css_named_colors.csv'
 
+// use csv method from d3-dsv
+
+d3.csv(CSV_URL).then(
+    data => {
+        let message = ''
+        message = message + Math.round(d3.csvFormat(data).length/1024) + ' kB\n'
+        message = message + data.length + ' rows\n'
+        message = message + data.columns.length + ' columns'
+        document.getElementById('message-container').textContent = message
+    }
+)
+// first/basic variant fetch
     // fetch(URL)
     //     .then((response) => response.text())
     //     .then(text=>console.log(text))
 
-    const fetchText = async (url) => {
-        const response = await fetch(url)
-        return await response.text()
+// second version with async
+    // const fetchText = async (url) => {
+    //     const response = await fetch(url)
+    //     return await response.text()
         
-    }
-    fetchText(csvURL).then(text => {
-        const data = d3.csvParse(text)
+    // }
+    // fetchText(CSV_URL).then(text => {
+    //     const data = d3.csvParse(text)
 
-        console.log(Math.round(text.length/1024) + ' kB')
-        console.log(data.length + ' rows')
-        console.log(data.columns.length + ' columns')
-    })
+    //     console.log(Math.round(text.length/1024) + ' kB')
+    //     console.log(data.length + ' rows')
+    //     console.log(data.columns.length + ' columns')
+    // })
   
     return (
         <div>
             <h2>Loading and parsing CSV data.</h2>
             <h3>Adding d3-dsv</h3>
             <a href="https://github.com/d3/d3-dsv">Link d3-dsv</a>
+            <pre id="message-container"></pre>
         </div>
     )
 }
