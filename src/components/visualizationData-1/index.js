aaa9638de65c2988acaaafa9a4a550cb09e7db99
Jorgen <ybeliaev@gmail.com>
2021-05-26 16:30:50 +0300

    chack length for word in <span>

--- src/components/visualizationData-1/index.js
+++ src/components/visualizationData-1/index.js
@@ -15,10 +15,24 @@ export default function VisualizationData1() {
         })
     },[])
     if(!data) return <pre>Loading..</pre>
-    console.log(data[0])
+    console.log("Data colors MDN: ", data[0])
     return (
         <div className='div_visual'>
-            {data.map(d =><div key={d['Keyword']} style={{backgroundColor: d['RGB hex value'] }}><span>{d['Keyword']}</span></div>)}
+            {data.map(d => {
+                if(d['Keyword'].length > 10){
+                    return (
+                        <div key={d['Keyword']} style={{backgroundColor: d['RGB hex value'] }}>
+                            <span style={{fontSize:"0.7rem"}}>{d['Keyword']}</span>
+                        </div>
+                    )
+                }else{
+                    return (
+                        <div key={d['Keyword']} style={{backgroundColor: d['RGB hex value'] }}>
+                            <span>{d['Keyword']}</span>
+                        </div>
+                    )
+                }
+            })}
         </div>
     )
 }
